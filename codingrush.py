861fb7b20f4a7eefaabfd5b92677a6f0374ae456
olgasofia1 <145036681+olgasofia1@users.noreply.github.com>
2023-09-19 09:33:00 -0600

    Create codingrush.py

---
+++ codingrush.py
@@ -0,0 +1,8 @@
+#código coding rush
+#OPCIÓN A 
+print("¿A qué temperatura estamos?")
+print("A 30 grados celcius ")
+fahrenheit=30*(9/5)+32
+print("Eso en farenheit es",fahrenheit)
+kelvin=30+273.15
+print("Y en kelvin sería",kelvin)
